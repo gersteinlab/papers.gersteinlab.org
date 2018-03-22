8ae30c856c40a7f86795af754b774d82a17910e9
Donghoon Lee <dhrlee@gmail.com>
2018-03-22 16:25:36 -0400

    returncode fix

--- cgi-bin/rebuild.py
+++ cgi-bin/rebuild.py
@@ -23,22 +23,22 @@ overallRC = 0
 if overallRC == 0:
     step1 = subprocess.Popen('/var/www/cgi-bin/1_gsheet2csv.py', stdout=subprocess.PIPE)
     print step1.stdout.read()
-    overallRC+=step1.returncode
+    overallRC+=int(step1.returncode)
 
 if overallRC == 0:
     step2 = subprocess.Popen('/var/www/cgi-bin/2_csv2xml.py', stdout=subprocess.PIPE)
     print step2.stdout.read()
-    overallRC+=step2.returncode
+    overallRC+=int(step2.returncode)
 
 if overallRC == 0:
     step3 = subprocess.Popen('/var/www/cgi-bin/3_xml2tsv.py', stdout=subprocess.PIPE)
     print step3.stdout.read()
-    overallRC+=step3.returncode
+    overallRC+=int(step3.returncode)
 
 if overallRC == 0:
     step4 = subprocess.Popen('/var/www/cgi-bin/4_update.py', stdout=subprocess.PIPE)
     print step4.stdout.read()
-    overallRC+=step4.returncode
+    overallRC+=int(step4.returncode)
 
 if overallRC == 0:
     print "Rebuild Complete!"
