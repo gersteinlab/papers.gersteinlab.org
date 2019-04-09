bca48501ecb2bee0cf40201cd603e801cc048076
Donghoon Lee <dhrlee@gmail.com>
2019-04-09 16:56:33 -0400

    added altmetric page

--- cgi-bin/4_update.py
+++ cgi-bin/4_update.py
@@ -25,6 +25,8 @@ subject_spreadsheet = list(csv.DictReader(open('../html/papers/update/subject_gs
 timestamp = str(datetime.now().ctime())
 
 summaryPath = "../html/papers/papers/"
+if not os.path.exists(summaryPath):
+    os.makedirs(summaryPath)
 summaryFile = open("../html/papers/index.html", 'w')
 
 simpleFile = open("../html/papers/simple.html", 'w')
@@ -34,62 +36,70 @@ subjectPath = "../html/papers/subject/"
 subjectFile = open("../html/papers/subject/index.html", 'w')
 subjectSummaryFile = open("../html/papers/subject/index.html", 'w')
 
-header = '''<link rel="stylesheet" href="/style.css">
-<!-- google analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+metricPath = "../html/papers/metric/"
+if not os.path.exists(metricPath):
+    os.makedirs(metricPath)
+metricFile = open("../html/papers/metric/index.html", 'w')
 
-  ga('create', 'UA-72218887-2', 'auto');
-  ga('send', 'pageview');
-</script>
-<!-- altmetric -->
-<script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>
-</HEAD>
-<BODY BGCOLOR="white">
-<div id="header">
-    <div id="global-title-container">
-        <div id="title-wrapper" class="clearfix">
-            <div id="title-left">
-                <a href="http://www.gersteinlab.org/"><strong>Gerstein</strong> Lab</a>
-            </div>
-            <div id="title-right">
-                <a href="http://cbb.yale.edu/">Bioinformatics</a>
+def makeHeader(title):
+    header = '''<HTML>
+    <HEAD>
+    <TITLE>'''+title+'''</TITLE>
+    <link rel="stylesheet" href="/style.css">
+
+    <!-- google analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-72218887-2', 'auto');
+      ga('send', 'pageview');
+    </script>
+
+    <!-- altmetric -->
+    <script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>
+
+    </HEAD>
+    
+    <BODY BGCOLOR="white">
+    <div id="header">
+        <div id="global-title-container">
+            <div id="title-wrapper" class="clearfix">
+                <div id="title-left">
+                    <a href="http://www.gersteinlab.org/"><strong>Gerstein</strong> Lab</a>
+                </div>
+                <div id="title-right">
+                    <a href="http://cbb.yale.edu/">Bioinformatics</a>
+                </div>
             </div>
         </div>
+
+        <CENTER>
+            <BR>
+            <!-- Buttons for individual sections -->
+            <FONT SIZE=+1 FACE='sans-serif, Arial, Helvetica' COLOR="#993333">
+                <SPAN CLASS="headerMenu"><A HREF="/">Main</A></SPAN>
+                &nbsp;&#8226;&nbsp;
+                <SPAN CLASS="headerMenu"><A HREF="/subject">By Subject</A></SPAN>
+                &nbsp;&#8226;&nbsp;
+                <SPAN CLASS="headerMenu"><A HREF="/metric">Metric</A></SPAN>
+                &nbsp;&#8226;&nbsp;
+                <SPAN CLASS="headerMenu"><A HREF="http://info.gersteinlab.org/Pubmed_query">Queries</A></SPAN>
+                &nbsp;&#8226;&nbsp;
+                <SPAN CLASS="headerMenu"><A HREF="http://info.gersteinlab.org/Papers_Page_Code">Code</A></SPAN>
+                &nbsp;&#8226;&nbsp;
+                <SPAN CLASS="headerMenu"><A HREF="http://wiki.gersteinlab.org/pubinfo/Paper_search">Search</A></SPAN>
+                &nbsp;&#8226;&nbsp;
+                <SPAN CLASS="headerMenu"><A HREF="http://wiki.gersteinlab.org/pubinfo/Other_Papers">Other Writings</A></SPAN>
+            </FONT>
+            <BR><BR>
+        </CENTER>
     </div>
-
-<CENTER>
-<!--
-<BR>
-<FONT SIZE=+2 FACE='sans-serif, Arial, Helvetica' COLOR="#334499">Gerstein Lab Publications</FONT>
-<BR>
--->
-<BR>
-<!-- Buttons for individual sections -->
-<FONT SIZE=+1 FACE='sans-serif, Arial, Helvetica' COLOR="#993333">
-    <SPAN CLASS="headerMenu"><A HREF="/">Main</A></SPAN>
-    &nbsp;&#8226;&nbsp;
-    <SPAN CLASS="headerMenu"><A HREF="/subject">By Subject</A></SPAN>
-    &nbsp;&#8226;&nbsp;
-    <SPAN CLASS="headerMenu"><A HREF="http://info.gersteinlab.org/Pubmed_query">Queries</A></SPAN>
-    &nbsp;&#8226;&nbsp;
-    <SPAN CLASS="headerMenu"><A HREF="http://info.gersteinlab.org/Papers_Page_Code">Code</A></SPAN>
-    &nbsp;&#8226;&nbsp;
-    <!--
-    <SPAN CLASS="headerMenu"><A HREF="http://wiki.gersteinlab.org/pubinfo/Paper_search">Search</A></SPAN>
-    &nbsp;&#8226;&nbsp;
-    -->
-    <SPAN CLASS="headerMenu"><A HREF="http://wiki.gersteinlab.org/pubinfo/Other_Papers">Other Writings</A></SPAN>
-</FONT>
-<BR><BR>
-</CENTER>
-
-</div>
-<div id="content">
-'''
+    <div id="content">
+    '''
+    return header
 
 def mergeData(master, pubmed):
     for row in master:
@@ -126,8 +136,8 @@ def write2csv(master):
             writer.writerow( (row['labid'],  row['PMID'], row['title'], row['citation'], row['preprint'], row['subject'], row['website'], row['Year'], row['footnote'], row['website2'], row['coreTool']) )
 
 def printLink(row):
-    out = ""
-    out += "<DIV CLASS=\"paperCite\">"
+
+    out = "<DIV CLASS=\"paperCite\">"
 
     ### add link to website
     if not row['website']:
@@ -147,23 +157,29 @@ def printLink(row):
     else:
         out += "<div id=\"medline\"><A HREF=\"http://www.ncbi.nlm.nih.gov:80/entrez/query.fcgi?cmd=Retrieve&db=PubMed&list_uids=" + row['PMID'].lstrip('\'') + "&dopt=Abstract\">medline</A></div>"
         
-        ### add altmetric badge
-        out += "<div id=\"altmetric\" data-badge-popover=\"right\" data-badge-type=\"4\" data-pmid=\"" + row['PMID'].lstrip('\'') + "\" data-condensed=\"true\" data-hide-no-mentions=\"true\" class=\"altmetric-embed\"></div>"
+        # ### add altmetric badge
+        # out += "<div id=\"altmetric\" data-badge-popover=\"right\" data-badge-type=\"4\" data-pmid=\"" + row['PMID'].lstrip('\'') + "\" data-condensed=\"true\" data-hide-no-mentions=\"true\" class=\"altmetric-embed\"></div>"
 
     out += "<div id=\"arrow-right\"><A HREF=\"/papers/" + row['labid'].lstrip('\'') + "/index-all.html\"></A></div>"
     out += "</DIV>\n<P style=\"margin: 35px;\" />\n"
     return out
 
-def printPapers(summaryFile, header):
-    out = "<HTML>\n"
-    out += "<HEAD>\n"
-    out += "<TITLE>Gerstein Lab Publications</TITLE>\n"
-    out += header
-    out += "\n"
-    out += "<CENTER>\n"
-    out += "<FONT FACE='sans-serif, Arial, Helvetica'><FONT SIZE=+2 COLOR=993333>Main Scientific Publications</FONT>\n"
-    out += "<H3><FONT SIZE=\"4\" COLOR=\"#334499\">Total papers: " + str(len(master_spreadsheet)) + "</FONT></H3></FONT>\n"
-    out += "(Last updated " + timestamp + ")</CENTER>\n"
+
+### MAIN FUNCTION
+def printPapers(summaryFile):
+    out = makeHeader("Gerstein Lab Publications")
+    out += '''
+    <CENTER>
+        <FONT FACE='sans-serif, Arial, Helvetica'><FONT SIZE=+2 COLOR=993333>Main Scientific Publications</FONT>
+            <H3>
+                <FONT SIZE="4" COLOR="#334499">
+                Total papers: ''' + str(len(master_spreadsheet)) + '''
+                </FONT>
+            </H3>
+        </FONT>
+        (Last updated ''' + timestamp + ''')
+    </CENTER>
+    '''
 
     years = {}
     currentYear = 0
@@ -179,27 +195,117 @@ def printPapers(summaryFile, header):
         pubmed = pubmed_spreadsheet
         if currentYear != row['Year']:
             currentYear = row['Year']
-            out += "<H3 ALIGN=center><FONT FACE='arial,helvetica,sans-serif' COLOR=000000 SIZE=4>-- " + currentYear + " ("+str(years[currentYear])+") --</FONT></H3>\n"
+            out += '''
+            <H3 ALIGN=center>
+                <FONT FACE='arial,helvetica,sans-serif' COLOR=000000 SIZE=4>-- ''' + currentYear + " ("+str(years[currentYear])+''') --</FONT>
+            </H3>
+            '''
             
         summaryFile.write(out)
         simpleFile.write(out)
         simpleFile2.write(out)
 
-        out = ""
+        out=""
+
+        # print each paper entry
         printPaperEntry(row, summaryFile, pubmed)
 
         # generate simple.html and papers-simple.html
         printSimpleEntry(row, simpleFile, pubmed)
         printSimpleEntry(row, simpleFile2, pubmed)
 
-        printEntrySummary(row, header, pubmed)
+        printEntrySummary(row, pubmed)
         printEntryExtended(row, pubmed)
+
 
     summaryFile.write("</DIV>\n</BODY>\n</HTML>")
     simpleFile.write("</DIV>\n</BODY>\n</HTML>")
-    simpleFile2.write("</DIV>\n</BODY>\n</HTML>")	
+    simpleFile2.write("</DIV>\n</BODY>\n</HTML>")
 
-def printEntrySummary(row, header, pubmed):
+def printPaperMetric(summaryFile):
+    out = makeHeader("Gerstein Lab Publications")
+    out += '''
+    <CENTER>
+        <FONT FACE='sans-serif, Arial, Helvetica'>
+            <FONT SIZE=+2 COLOR=993333>Metrics for Gerstein Lab Publications</FONT>
+            <H3>
+                <FONT SIZE="4" COLOR="#334499">
+                Total papers: ''' + str(len(master_spreadsheet)) + '''
+                </FONT>
+            </H3>
+        </FONT>
+        (Last updated ''' + timestamp + ''')
+    </CENTER>
+    <table style="width:80%" align="center" cellpadding="5">
+    '''
+
+    years = {}
+    currentYear = 0
+
+    for row in master_spreadsheet:
+        rowYear = row['Year']
+        if years.has_key(rowYear):
+            years[rowYear] += 1
+        else:
+            years[rowYear] = 1
+
+    for row in master_spreadsheet:
+        pubmed = pubmed_spreadsheet
+        if currentYear != row['Year']:
+            currentYear = row['Year']
+            out += '''
+            <tr valign="bottom">
+            <th></th>
+            <th>
+            <H3 align="left">
+                <br><br>
+                <i>
+                    <FONT FACE='arial,helvetica,sans-serif' COLOR=000000 SIZE=4>''' + currentYear + " ("+str(years[currentYear])+''')</FONT>
+                </i>
+            </H3>
+            </th>
+            </tr>
+            '''
+        out += '''
+        <tr align=left valign=middle>
+            <th>
+                <div id="altmetric" data-badge-popover="right" data-badge-type="donut" data-pmid="''' + row['PMID'].lstrip('\'') + '''" data-condensed="true" data-hide-no-mentions="true" class="altmetric-embed"></div>
+            </th>
+            <th>
+        '''
+
+        # print title and citation
+        if row['title']:
+            out += '''
+            <DIV CLASS="paperTitle">
+                <FONT SIZE=+1>
+                    <B><A HREF="/papers/''' + row['labid'].lstrip('\'') + '''/index.html">''' + row['title'].lstrip('\'') + '''</A></B>
+                </FONT>
+            </DIV>'''
+        else:
+            print "Warning: Title Empty!"
+            print row
+
+        if row['citation']:
+            out += row['citation'].lstrip('\'')
+        else:
+            print "Warning: Citation Empty!"
+            print row
+
+        out += '''
+            </th>
+        </tr>
+        '''
+    
+    out += '''
+    </table>
+    </DIV>
+    </BODY>
+    </HTML>
+    '''
+    metricFile.write(out)
+
+def printEntrySummary(row, pubmed):
 
     # create summary directory
     entrySummary = summaryPath + row['labid'].lstrip('\'')
@@ -208,8 +314,7 @@ def printEntrySummary(row, header, pubmed):
     entrySummaryIndex = entrySummary + "/index.html"
     entrySummaryFile = open(entrySummaryIndex, 'w')
 
-    out = "<HTML>\n<HEAD>\n<TITLE>"+row['title'].lstrip('\'')+"</TITLE>"
-    out += header
+    out = makeHeader(row['title'].lstrip('\''))
     out += "<blockquote>\n<font size=+3><tt>" + row['labid'] + "</tt></font><p>\n"
 
     # print title and citation
@@ -231,11 +336,7 @@ def printEntryExtended(row, pubmed):
     entryExtendedIndex = entryExtended + "/index-all.html"
     entryExtendedFile = open(entryExtendedIndex, 'w')
 
-    out = "<HTML>\n"
-    out += "<HEAD>\n"
-    out += "<TITLE>" + row['title'].lstrip('\'') + "</TITLE>"
-    out += header
-    out += "\n"
+    out = makeHeader(row['title'].lstrip('\''))
     out += "<blockquote>\n"
     if not row['labid']:
         out += "<font size=+3><tt>" + row['PMID'].lstrip('\'') + "</tt></font><p>"
@@ -377,7 +478,7 @@ def printSimpleEntry(row, simpleFile, pubmed):
 
     simpleFile.write(out)
 
-def printSubject(master_spreadsheet, header):
+def printSubject(master_spreadsheet):
     allsubjects = []
     subjectpapers = []
     for i, row in enumerate(master_spreadsheet):
@@ -400,12 +501,8 @@ def printSubject(master_spreadsheet, header):
                 os.makedirs(path)
             subjectFile = open(path + '/index.html', 'w')
 
-            out = '''<HTML>
-<HEAD>
-<TITLE>%s</TITLE>
-''' % subject.capitalize()
+            out = makeHeader(subject.capitalize())
 
-            out += header
             include = ''
             if row['HTML']:
                 include = row['HTML'].lstrip('\'')
@@ -425,11 +522,8 @@ def printSubjectFile(subject, subject_papers):
     subjectDetailFile = open(path + '/index.html', 'w')
 
     #	subjectInclude = path + '/include.html'
-    out = '''<HTML>
-<HEAD>
-<TITLE>%s</TITLE>
-''' % subject.capitalize()
-    out += header
+
+    out = makeHeader(subject.capitalize())
 
     # include include file if exists
     #	if os.path.exists(subjectInclude):
@@ -464,16 +558,14 @@ def printSubjectEntry(row, subjectDetailFile):
 
     subjectDetailFile.write(out)
 
-def printSubjectSummary(subject_spreadsheet, header):
+def printSubjectSummary(subject_spreadsheet):
 
     category = ''
     SubjectNumber = 0
     outPart = ''
-    out = "<HTML>\n"
-    out += "<HEAD>\n"
-    out += "<TITLE>Gerstein Lab Publications</TITLE>\n"
-    out += header
-    out += "\n"
+
+    out = makeHeader("Gerstein Lab Publications")
+    
     out += "<BR>"
     out += '<BODY onload="javascript:showdh(3);">\n'
     out += '<TABLE width="915" height="30" frame ="hsides" align="center" cellpadding="0" cellspacing="0">\n<TR>\n<TD width="6"></TD>\n'
@@ -519,13 +611,16 @@ print "Saving merged data as CSV file: merged.csv"
 write2csv(master_spreadsheet)
 
 print "Building Subject Summary HTML files.."
-printSubjectSummary(subject_spreadsheet, header)
+printSubjectSummary(subject_spreadsheet)
 
 print "Building Subject HTML files.."
-printSubject(master_spreadsheet, header)
+printSubject(master_spreadsheet)
 
 print "Building Paper HTML files.."
-printPapers(summaryFile, header)
+printPapers(summaryFile)
+
+print "Building Paper metric HTML files.."
+printPaperMetric(summaryFile)
 
 # Fix permissions to make both apache and sudo users run updates
 print "Fixing permissions for HTML files.."
